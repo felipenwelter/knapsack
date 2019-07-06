8980254af671f539a2fdb3951fe6c79c3327c241
Vitor Emanuel Batista <vitorebatista@gmail.com>
2019-07-06 16:24:43 -0300

    lint population

--- population.py
+++ population.py
@@ -5,22 +5,29 @@ import operator
 import config
 from cromossomo import Cromossomo
 
+
 def sortPopulation(newList):
     '''ordena primeiro os valores fitness = true, depois value, depois weight, por exemplo:
     true/19/29 - true/19/28 - true/18/30 - true 18/25 - true/18/25 - false/26/50'''
-    sortedList = sorted(newList, key=operator.attrgetter('weight'),reverse=True)
-    sortedList = sorted(sortedList, key=operator.attrgetter('value'), reverse=True)
-    sortedList = sorted(sortedList, key=operator.attrgetter("fitness"),reverse=True)
+    sortedList = sorted(
+        newList, key=operator.attrgetter('weight'), reverse=True)
+    sortedList = sorted(
+        sortedList, key=operator.attrgetter('value'), reverse=True)
+    sortedList = sorted(
+        sortedList, key=operator.attrgetter("fitness"), reverse=True)
     return sortedList
 
 
 class Population:
     '''Classe que representa a população de uma geração com um determinado número de indivíduos (cromossomos)'''
+
     def __init__(self):
-        self.cromossomos = [] #armazena os indivíduos (classe cromossomo)
-        self.size = config.population_size #define o número de invíduos da população
-        self.weightAverage = 0 #a média de peso dos indivíduos da população (informativo)
-        self.procreateMethod = config.procreateMethod # método de procriação (geração de nova população)
+        self.cromossomos = []  # armazena os indivíduos (classe cromossomo)
+        self.size = config.population_size  # define o número de invíduos da população
+        # a média de peso dos indivíduos da população (informativo)
+        self.weightAverage = 0
+        # método de procriação (geração de nova população)
+        self.procreateMethod = config.procreateMethod
 
     def initialize(self):
         '''inicializa a população de cromossomos com valores aleatórios'''
@@ -36,17 +43,18 @@ class Population:
         for c in self.cromossomos:
             c.evaluate_fitness()
             self.weightAverage += c.weight
-        self.weightAverage = round(self.weightAverage/self.size,2)
+        self.weightAverage = round(self.weightAverage/self.size, 2)
 
     def print(self):
         '''Imprime em tela cada cromossomo da população e suas características principais '''
-        #ordena pelo maior valor
+        # ordena pelo maior valor
         sortedList = sortPopulation(self.cromossomos)
         for c in sortedList:
-            print(f"composition {c.composition} weight {c.weight} | value {c.value} | %limit Weight {c.usedWeightPercent} | fitness {c.fitness}")
+            print(
+                f"composition {c.composition} weight {c.weight} | value {c.value} | %limit Weight {c.usedWeightPercent} | fitness {c.fitness}")
         print(f" population weight average is {self.weightAverage} kg")
 
-    def procreate(self,ancestral):
+    def procreate(self, ancestral):
         '''Geração de uma nova população a partir de uma população ancestral. Permite três formas diferentes:
         - random: a seleção dos progenitores é feita de forma aleatória
         - all_elite: todos os elementos que atendem ao critério fitness são copiados para a próxima geração
@@ -55,14 +63,14 @@ class Population:
 
         if (self.procreateMethod == "random"):
             for i in range(self.size):
-                #não replica nenhum item da população ancestral para a nova população
+                # não replica nenhum item da população ancestral para a nova população
                 p1, p2 = self.selectParents(ancestral.cromossomos)
-                new_cromossomo = self.crossover(p1,p2)
+                new_cromossomo = self.crossover(p1, p2)
                 new_cromossomo.mutate()
                 self.cromossomos.append(new_cromossomo)
 
         elif (self.procreateMethod == "all_elite"):
-            #replica para a nova população todos os cromossomos que atendem criterio de fitness
+            # replica para a nova população todos os cromossomos que atendem criterio de fitness
             selected = []
             newList = sortPopulation(ancestral.cromossomos)
             for i in range(self.size):
@@ -71,46 +79,50 @@ class Population:
                 else:
                     break
 
-            #completa a população fazendo crossover dos cromossomos selecionados
-            missingCromossomos = len(ancestral.cromossomos) - len(self.cromossomos)
-            for i in range( missingCromossomos ):
-                p1, p2 = self.selectParents(selected if len(selected) > 0 else ancestral.cromossomos )
-                new_cromossomo = self.crossover(p1,p2)
-                new_cromossomo.mutate() #mutação do novo cromossomo
+            # completa a população fazendo crossover dos cromossomos selecionados
+            missingCromossomos = len(
+                ancestral.cromossomos) - len(self.cromossomos)
+            for i in range(missingCromossomos):
+                p1, p2 = self.selectParents(selected if len(
+                    selected) > 0 else ancestral.cromossomos)
+                new_cromossomo = self.crossover(p1, p2)
+                new_cromossomo.mutate()  # mutação do novo cromossomo
                 self.cromossomos.append(new_cromossomo)
 
         elif (self.procreateMethod == "first_elite"):
-            #replica para a nova população somente o melhor cromossomo que atende ao criterio de fitness
+            # replica para a nova população somente o melhor cromossomo que atende ao criterio de fitness
             selected = []
             newList = sortPopulation(ancestral.cromossomos)
             if (newList[0].fitness == True):
                 selected.append(newList[0])
                 self.cromossomos.append(newList[0])
 
-            #completa a população fazendo crossover dos cromossomos selecionados
-            missingCromossomos = len(ancestral.cromossomos) - len(self.cromossomos)
-            for i in range( missingCromossomos ):
-                p1, p2 = self.selectParents(selected if len(selected) > 0 else ancestral.cromossomos )
-                new_cromossomo = self.crossover(p1,p2)
-                new_cromossomo.mutate() #mutação do novo cromossomo
+            # completa a população fazendo crossover dos cromossomos selecionados
+            missingCromossomos = len(
+                ancestral.cromossomos) - len(self.cromossomos)
+            for i in range(missingCromossomos):
+                p1, p2 = self.selectParents(selected if len(
+                    selected) > 0 else ancestral.cromossomos)
+                new_cromossomo = self.crossover(p1, p2)
+                new_cromossomo.mutate()  # mutação do novo cromossomo
                 self.cromossomos.append(new_cromossomo)
 
-    def selectParents(self,cromossomoList):
+    def selectParents(self, cromossomoList):
         '''Seleciona os pais para realizar o cruzamento. Busca os pais a partir de uma lista
         restrita de cromossomos de uma população anterior enviada como parâmetro.'''
         limit = len(cromossomoList)
-        r = random.randint(0,limit-1)
+        r = random.randint(0, limit-1)
         p1 = cromossomoList[r]
-        s = random.randint(0,limit-1)
+        s = random.randint(0, limit-1)
         p2 = cromossomoList[s]
         return p1, p2
 
-    def crossover(self,p1,p2):
+    def crossover(self, p1, p2):
         '''Faz cruzamento (crossover) unindo a primeira metade de genes do primeiro progenitor
         com a segunda meteade de genes do segundo progenitor'''
         half = int(p1.compositionSize/2)
         new_composition = p1.composition[:half] + p2.composition[half:]
         c = Cromossomo()
         c.composition = new_composition
-        c.evaluate_fitness() #atualiza características do cromossomo
+        c.evaluate_fitness()  # atualiza características do cromossomo
         return c
