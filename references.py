4bdfef3c40a2854957bd5d3840970f675f5c93ee
Vitor Emanuel Batista <vitorebatista@gmail.com>
2019-07-07 12:41:44 -0300

    add references

--- references.py
+++ references.py
@@ -1,7 +1,8 @@
 #Teoria:
 #http://www.inf.ufsc.br/~alexandre.goncalves.silva/courses/14s2/ine5633/slides/aulaAG_aplicacoes.pdf
 #https://pt.slideshare.net/biancamcdantas/algoritmos-genticos-aplicados-ao-problema-da-mochila-multidimensional
-
+# http://www.es.ele.tue.nl/education/5MC10/Solutions/knapsack.pdf
+# ftp://ftp.dca.fee.unicamp.br/pub/docs/vonzuben/theses/pvargas_mest/arq_11.pdf
 
 #Exemplo usando DEAP:
 ## https://deap.readthedocs.io/en/master/examples/index.html
